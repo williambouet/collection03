85c8129e2bec9e6d9fe1eef06060924eb5eec835
Bastien Krettly <bastien@wildcodeschool.fr>
2019-06-12 09:15:56 +0200

    Update Thanos.java

--- Thanos.java
+++ Thanos.java
@@ -25,7 +25,7 @@ public class Thanos {
             System.out.println(hero.getName() + ", " + hero.getAge());
         }
 
-        // TODO 3: Show heroes by age (ascending),from Spider-Man (include) to Iron Man (exclude)
+        // TODO 3: Show heroes by age (ascending),from Spider-Man (include) to Iron Man (include)
         Hero spiderman = new Hero("Spider-Man", 18);
         Hero ironman = new Hero("Iron Man", 48);
         heroes.add(spiderman);
