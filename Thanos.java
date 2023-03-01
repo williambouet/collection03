17fc97110af3a759d5d6d732c417dd1f3a1ac668
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-03-01 11:40:14 +0100

    quest done

--- Thanos.java
+++ Thanos.java
@@ -13,26 +13,26 @@ public class Thanos {
         heroes.add(new Hero("Scarlet Witch", 29));
         heroes.add(new Hero("Doctor Strange", 42));
 
-        // TODO 1 : Retrieve and remove the oldest hero in one line
-        Hero oldest = null;
+        //  1 : Retrieve and remove the oldest hero in one line
+        Hero oldest = heroes.pollLast();
 
         System.out.println("\nOldest hero:");
         System.out.println(oldest == null ? "" : oldest.getName());
 
-        // TODO 2: Show heroes by age (descending)
+        // 2: Show heroes by age (descending)
         System.out.println("\nHeroes by age (descending) :");
-        for (Hero hero : heroes) {
+        for (Hero hero : heroes.descendingSet()) {
             System.out.println(hero.getName() + ", " + hero.getAge());
         }
 
-        // TODO 3: Show heroes by age (ascending),from Spider-Man (include) to Iron Man (include)
+        //  3: Show heroes by age (ascending),from Spider-Man (include) to Iron Man (include)
         Hero spiderman = new Hero("Spider-Man", 18);
         Hero ironman = new Hero("Iron Man", 48);
         heroes.add(spiderman);
         heroes.add(ironman);
 
         System.out.println("\nSubset of heroes :");
-        for (Hero hero : heroes) {
+        for (Hero hero : heroes.subSet(spiderman, true, ironman, true)) {
             System.out.println(hero.getName() + ", " + hero.getAge());
         }
     }
---
+++ challenge.md
@@ -0,0 +1,29 @@
+Thanos possède un TreeSet de Heroes, implémentant Comparable avec un tri par age croissant.
+
+En une seule ligne, récupère et supprime le héros le plus vieux de l'ensemble
+Sans modifier l'ensemble d'origine, affiche la liste des héros par âge décroissant
+Sans modifier l'ensemble d'origine, affiche la liste des héros par âge croissant, allant de Spider-Man (compris) à Iron Man (compris)
+Résultat attendu lors de l'exécution de Thanos :
+
+$ Oldest hero:
+$ Thor
+$ 
+$ Heroes by age (descending) :
+$ Captain America, 100
+$ Hulk, 49
+$ Doctor Strange, 42
+$ Black Widow, 34
+$ Scarlet Witch, 29
+$ Vision, 3
+$ 
+$ Subset of heroes :
+$ Spider-Man, 18
+$ Scarlet Witch, 29
+$ Black Widow, 34
+$ Doctor Strange, 42
+$ Iron Man, 48
+
+Critères de validation
+Le dépôt GitHub contient bien les fichiers Hero.java et Thanos.java
+Seule la classe Thanos doit être modifiée
+La classe Thanos se compile sans erreur et affiche dans le terminal un résultatidentique à celui présenté précédemment
